473f186f11bc8356fb3912a414a0a0d364f5e490
Felipe Reyes <felipe.reyes@canonical.com>
2020-05-06 07:05:25 -0400

    Initial import

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="typing",
+    version="3.9999",
+    author="Felipe Reyes",
+    author_email="freyes@tty.cl",
+    packages=setuptools.find_packages())
